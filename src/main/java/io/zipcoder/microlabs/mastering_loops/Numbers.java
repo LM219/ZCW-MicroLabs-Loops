b30a0d4535927c6e8480834b4ba2447761ee15e0
Milligan <E824095@EMEA.AD.JPMORGANCHASE.com>
2017-08-10 09:44:24 +0100

    Have not completed Question 10 yet

--- src/main/java/io/zipcoder/microlabs/mastering_loops/Numbers.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Numbers.java
@@ -1,18 +1,36 @@
 package io.zipcoder.microlabs.mastering_loops;
+import java.util.*;
 
 
 public class Numbers {
 
     public String oneToTen(){
-        return "";
+    	String output = "";
+    	//name of the fucntion
+    	output += "oneToTen()\n";
+    	output += "*** Output ***";
+    	for (int x=1; x<=10; x++)
+    	{
+    		output += "\n" + x; 	
+    		
+    	}
+        return output;
     }
 
     public String oddNumbers(){
-        return "";
+    	String output = "oddNumbers()\n" + "*** Output ***";
+    			for (int x=1; x<20; x+=2) {
+    				output += "\n" + x;
+    			}
+        return output;
     }
 
     public String squares(){
-        return "";
+    	String output = "squares()\n" + "*** Output ***";
+    	for (int x=1; x<=10; x++) {
+    		output += "\n" + x*x;
+    	}
+        return output;
     }
 
     public String random4(){
@@ -20,11 +38,85 @@ public class Numbers {
     }
 
     public String even(int n){
-        return "";
+    	String output = "even()\n" + "*** Output ***";
+    	for (int x=2; x<=n; x+=2) {
+			output += "\n" + x;
+    	}
+ 
+        return output;
     }
 
     public String powers(int n){
-        return "";
+    	String output = "powers()\n" + "*** Output ***";
+    	for (int x=1; x<=n; x+=1)
+    	{
+    		output += "\n" + (int) Math.pow(2, x);   
+    	}
+        return output;
     }
+    
+    public String areWeThereYet(String n) {
+    	if (n.equalsIgnoreCase("yes")) 
+    	{
+    		return "Good!";
+    	
+    		}
+    	else {
+    		return "Are we there yet?";
+    		
+    	}
+    }
+    	
 
+    
+    public String triangle(String s, int n) {
+    	String mytriangle = s;
+    	String output= s;
+    	
+    	for (int x=1; x<n; x++){
+    		mytriangle = mytriangle + s; 	
+    		output = output + "\n" + mytriangle;
+    		
+    	}		
+    	return output;
+    }  
+    
+    public String testSquare() {
+    	String output= "";
+    	for (int i=1; i<=4; i++){
+    		String value = "";
+    		for (int j=1; j<=4; j++)
+    		{
+    			if((i*j)<10)
+    			 value = value + "|  " + (i*j) + " ";
+    			else
+    				value = value + "| " + (i*j) + " ";
+    		
+    		}
+    		output= output + "\n" + value + "|";
+    				
+    	}
+    	return output;
+    }
+    
+   
+    //THIS QUESTION HAS NOT BEEN COMPLETED YET
+    public String testSquaresn(int row, int column) {
+    	String output= "";
+    	for (int i=1; i<=row; i++){
+    		String value = "";
+    		for (int j=1; j<=column; j++)
+    		{
+    			if((i*j)<10)
+    			 value = value + "|" + (i*j) + " ";
+    			else
+    				value = value + " | " + (i*j) + " ";
+    		
+    		}
+    		output= output + value + "\n" + "|";
+    				
+    	}
+    	return output;
+    }
+    
 }
--- src/test/java/io/zipcoder/microlabs/mastering_loops/NumbersTest.java
+++ src/test/java/io/zipcoder/microlabs/mastering_loops/NumbersTest.java
@@ -9,7 +9,7 @@ public class NumbersTest {
     public void oneToTenTest(){
         //: Given
         Numbers numbers = new Numbers();
-        String expected = "*** Output ***\noneToTen()\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
+        String expected = "oneToTen()\n*** Output ***\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
 
         //: When
         String actual = numbers.oneToTen();
@@ -21,25 +21,32 @@ public class NumbersTest {
     @Test
     public void oddNumbersTest(){
         //: Given
-
+    	Numbers numbers = new Numbers ();
+    	String expected ="oddNumbers()\n*** Output ***\n1\n3\n5\n7\n9\n11\n13\n15\n17\n19";
         //: When
-
+    	String actual = numbers.oddNumbers();
         //: Then
+    	Assert.assertEquals("The two strings are equal" , expected, actual);
     }
 
     @Test
     public void squaresTest(){
         //: Given
+    	Numbers numbers = new Numbers();
+    	String expected = "squares()\n*** Output ***\n1\n4\n9\n16\n25\n36\n49\n64\n81\n100";
 
         //: When
+    	String actual = numbers.squares();
 
         //: Then
+    	Assert.assertEquals("The two strings are equals", expected, actual);
     }
 
     @Test
     public void random4Test(){
         //: Given
-
+    	Numbers numbers = new Numbers();
+    	//String expected = "random4()\n*** Output ***\n" + int +
         //: When
 
         //: Then
@@ -48,19 +55,73 @@ public class NumbersTest {
     @Test
     public void evenTest(){
         //: Given
-
+    	Numbers numbers = new Numbers();
+    	String expected = "even()\n*** Output ***\n2\n4\n6\n8\n10";
         //: When
-
+    	String actual = numbers.even(10);
+    	
         //: Then
+    	Assert.assertEquals("The two strings are equals", expected, actual);
     }
 
     @Test
     public void powersTest(){
         //: Given
-
+    	Numbers numbers = new Numbers();
+    	String expected = "powers()\n*** Output ***\n2\n4\n8";
         //: When
-
+    	String actual = numbers.powers(3);
         //: Then
+    	Assert.assertEquals("The two strings are equals", expected, actual);
     }
 
+    @Test    
+    public void areWeThereYetTest(){
+        //: Given
+    	Numbers numbers = new Numbers();
+    	String expected = "Good!";
+        //: When
+    	String actual = numbers.areWeThereYet("yes");
+        //: Then
+    	Assert.assertEquals("The two strings are equals", expected, actual);
+    }
+    
+    @Test    
+    public void triangle(){
+        //: Given
+    	Numbers numbers = new Numbers();
+    	String expected = "*\n**\n***\n****\n*****";
+//        //: When
+    	String actual = numbers.triangle("*", 5);
+//        //: Then
+    	Assert.assertEquals("The two strings are equals", expected, actual);
+    	
+    	//numbers.triangle("", 5);
+    }
+    
+    @Test    
+    public void testSquare(){
+        //: Given
+    	Numbers numbers = new Numbers();
+        String expected = "\n|  1 |  2 |  3 |  4 |\n|  2 |  4 |  6 |  8 |\n|  3 |  6 |  9 | 12 |\n|  4 |  8 | 12 | 16 |";
+    	//String expected = " 1 2 3  4 \n  2  4  6  8 \n  3  6  9  12 \n  4  8  16  20 ";
+
+        //: When
+    	String actual = numbers.testSquare();
+        //: Then
+    	Assert.assertEquals("The two strings are equals", expected, actual);
+    }
+    
+    @Test    
+    public void testSquaresn(){
+        //: Given
+    	Numbers numbers = new Numbers();
+        String expected = "\n|  1 |  2 |  3 |  4 |\n|  2 |  4 |  6 |  8 |\n|  3 |  6 |  9 | 12 |\n|  4 |  8 | 12 | 16 |";
+    	//String expected = " 1 2 3  4 \n  2  4  6  8 \n  3  6  9  12 \n  4  8  16  20 ";
+
+        //: When
+    	String actual = numbers.testSquaresn(4,4);
+        //: Then
+    	Assert.assertEquals("The two strings are equals", expected, actual);
+    }
 }
